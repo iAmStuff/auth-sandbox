3b23b3f9ff5c0e9aec70be774728cc0c9ad2c9ec
Quentin Sothern <quentinsothern@developersinstitute.co.nz>
2021-03-02 16:03:55 +1300

    why were passwords unique? what dumbass has been copy-pasting code smh my head

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const model = new Schema({
   username: { type: String, required: true },
   userid: { type: String, required: true, unique: true },
-  password: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   hint: { type: String },
 });
 
